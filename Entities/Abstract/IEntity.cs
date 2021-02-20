e24a54101ad44095fcc678cfcb18bbd8883e1cd7
Mehmet CEYLAN <ceylarino@gmail.com>
2021-02-20 13:53:12 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implemented classes are database "table"s
+    public interface IEntity
+    {
+    }
+}
