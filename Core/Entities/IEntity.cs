de264f4d08290915a89e81852f246ed4f1727bba
Mehmet CEYLAN <ceylarino@gmail.com>
2021-02-21 15:01:32 +0300

    Code refactoring: Added generic "Core" layer.

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //IEntity implemented classes are database "table"s
     public interface IEntity
